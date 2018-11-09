e404cb708cdd110fb805c2498bf71f2e32a9111a
solareenlo <solareenlo@gmail.com>
2018-11-09 17:15:22 +0900

    別ファイルの関数とかを飛ばせるdefaultの使い方

---
+++ 09_module/default/add.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export let someone = {
+  name: "johndoe",
+  age: 30
+};
+
+export default {
+  name: "Tom",
+  add: add
+}
---
+++ 09_module/default/index.js
@@ -0,0 +1,7 @@
+// default
+import myAll from "./add";
+
+console.log(myAll);
+
+let value = myAll.add(12, 15);
+console.log(value);
