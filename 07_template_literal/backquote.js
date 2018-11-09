29b914f67525a37721286ea8596408dc8754ea0e
solareenlo <solareenlo@gmail.com>
2018-11-09 13:20:22 +0900

    バッククオート内でのスペースや\nやタブや$の表示の仕方について

--- 07_template_literal/backquote.js
+++ 07_template_literal/backquote.js
@@ -11,3 +11,19 @@ let message1 = `私の名前は${name}で、${age}歳です。`;
 console.log(message1);
 
 // この${}のことをエクスプレッション(expression)と言います.
+
+
+
+// 改行のやり方
+let message2 = "私の名前は" + name + "で、\n" + age + "歳です。";
+console.log(message2);
+let message3 = `私の名前は${name}で、
+${age}歳です。`;
+console.log(message3);
+
+
+
+// バッククオートの中では、打ったものがそのままで出てきます.
+let message4 = `私の名前は\${name}   で、
+  ${age}歳\nです。`;
+console.log(message4);
