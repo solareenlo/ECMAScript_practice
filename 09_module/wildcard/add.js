bcdf20ebe7f4b07f271c7cf7873d6940114609b0
solareenlo <solareenlo@gmail.com>
2018-11-09 17:04:09 +0900

    ワイルドカードを使って、別ファイルの関数とかをひとまとめにする

---
+++ 09_module/wildcard/add.js
@@ -0,0 +1,12 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export let someone = {
+  name: "johndoe",
+  age: 30
+};
---
+++ 09_module/wildcard/index.js
@@ -0,0 +1,6 @@
+// ワイルドカード(*)
+import * as myAll from "./ass";
+let value = myAll.sub(3, 2);
+
+console.log(value);
+console.log(myAll.someone);
