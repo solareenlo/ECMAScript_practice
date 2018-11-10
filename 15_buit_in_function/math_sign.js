8aca4e76b19f496e0947c2b684fedc62c19ba4d5
solareenlo <solareenlo@gmail.com>
2018-11-10 15:56:25 +0900

    プラスは1, 0は0, マイナスは-1を返すmath.sign

---
+++ 15_buit_in_function/math_sign.js
@@ -0,0 +1,10 @@
+// Math.sign()
+
+console.log(Math.sign());
+console.log(Math.sign(1));
+console.log(Math.sign(111));
+console.log(Math.sign(111.111));
+console.log(Math.sign(0));
+console.log(Math.sign(-1));
+console.log(Math.sign(-1342));
+console.log(Math.sign(-12.231));
